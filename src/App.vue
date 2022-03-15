73ed6620ed4f7610262daf346249f21a3ca3d0ba
zkx <991992844@qq.com>
2022-03-15 16:11:53 +0800

    组件间的通信以及 tree notice form三个组件的封装

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,42 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <!--      <HelloWorld msg="Welcome to Your Vue.js App"/>-->
+          <CompCommunicate />
+<!--    <SlotsTest></SlotsTest>-->
+    <!-- <Recursion></Recursion> -->
+    <!--<FormSample></FormSample>-->
+    <!--<Tree></Tree>-->
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+  // import HelloWorld from "./components/HelloWorld.vue";
+  import CompCommunicate from "./components/communicate";
+  // import SlotsTest from "./components/slots";
+  // import Recursion from "./components/recursion";
+  // import FormSample from "./components/form";
+  // import Tree from "./components/tree";
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  export default {
+    name: "app",
+    components: {
+      // HelloWorld,
+      CompCommunicate,
+      // SlotsTest,
+      // Recursion,
+      // FormSample,
+      // Tree
+    }
+  };
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  #app {
+    font-family: "Avenir", Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+  }
 </style>
---
+++ src/components/slots/Comp3.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+        <h3>
+            作用域插槽
+        </h3>
+        <!-- 通过绑定指定作用域 -->
+        <slot :foo="foo"></slot>
+    </div>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {
+                foo: 'comp3的数据'
+            }
+        },
+    }
+</script>
+
+<style scoped>
+
+</style>
