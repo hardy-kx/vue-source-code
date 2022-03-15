766a8f9ac5c1d370b33f8e82ed0782a0a04260c9
zkx <991992844@qq.com>
2022-03-15 17:24:13 +0800

    全局注册组件 注释

--- src/utils/create.js
+++ src/utils/create.js
@@ -1,19 +1,22 @@
 import Vue from 'vue';
 
+// create把传递的组件配置转换为组件实例返回
 export default function create(Component, props) {
-    // 先创建实例
+    // 先创建vue实例，用它创建组件实例
     const vm = new Vue({
         render(h) {
             // h就是createElement，它返回VNode
             return h(Component, {props})
         }
-    }).$mount();
+    }).$mount(); // $mount里面会调render生成VNode，生成的VNode会执行update函数生成DOM
 
-    // 手动挂载
+    // 手动挂载：生成DOM结构存储在vm.$el把它追加到body即可
     document.body.appendChild(vm.$el);
 
+    // 从vm.$children中拿出comp
+    const comp = vm.$children[0]; // vm.$root也是comp
+
     // 销毁方法
-    const comp = vm.$children[0];
     comp.remove = function() {
         document.body.removeChild(vm.$el);
         vm.$destroy();
